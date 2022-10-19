e034fd33790a7a8335a8f0d69b62a57fdc8dca00
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-19 09:20:32 +0300

    Add rubocop linter

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'greeting', to: 'greetings#index'
-
-      #  resources :greetings, only: [:index]
     end
   end
-  # root "home#index"
-  # get '/*path' => 'home#index'
 end
