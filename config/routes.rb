48e42586875bc7099ea9cefbe94e0d12eed840ce
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-10-19 14:13:19 +0300

    Route changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'greeting', to: 'greetings#index'
-
-      #  resources :greeting, only: [:index]
     end
   end
+  root "api/v1/greetings#index"
 end
